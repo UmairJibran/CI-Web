72eda96070dd035742823aa4b254bbc4709cf6bc
Umair Jibran <umairjibran7@gmail.com>
2020-07-05 06:42:15 +0500

    added base url, and imported bootstrap+jquery

---
+++ application/controllers/Home.php
@@ -0,0 +1,8 @@
+<?php
+   class Home extends CI_Controller {
+      public function index()	{
+         $data['main_view'] = "home_view";
+         $this->load->view('layout/main',$data);
+      }
+   }
+?>
---
+++ application/views/layout/main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+   <link rel="stylesheet" href="<?php echo base_url()?>/assets/css/bootstrap.min.css">
+   <link rel="stylesheet" href="<?php echo base_url()?>/assets/css/master.css">
+</head>
+<body>
+   <?php $this->load->view($main_view); ?>
+   <script src="<?php echo base_url()?>/assets/js/jquery.js"></script>
+   <script src="<?php echo base_url()?>/assets/js/bootstrap.min.js"></script>
+</body>
+</html>
