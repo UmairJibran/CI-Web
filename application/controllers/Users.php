fb5a02f1f89836e2dc939d6e6346d67969992eaf
Umair Jibran <umairjibran7@gmail.com>
2020-07-05 14:03:20 +0500

    trying sessions

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -1,38 +1,52 @@
 <?php
    class Users extends CI_Controller{
-      public function index(){
-         $this->load->view('welcome_message');
+      public function login(){
+         $this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[4]');
+         $this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[4]');
+         if($this->form_validation->run() == FALSE){
+            $data = array(
+               'errors' => validation_errors(),
+            );
+            $this->session->set_flashdata($data);
+            print ($this->session->flashdata('errors'));
+            // redirect("home");
+         }
+         // $username = $this->input->post('username');
+         // $password = $this->input->post('password');
       }
-      public function show($user){
-         $data['result'] = $this->user_model->getUsers($user,1234);
-         $this->load->view('user_view',$data);
-      }
-      public function addUser(){
-         $username = 'spiderman';
-         $fullName = 'Peter Parker';
-         $password = 'ironman';
-         $this->user_model->createUser(
-            [
-               'user_username'=>$username,
-               'user_full_name'=>$fullName,
-               'user_password'=>$password,
-            ],
-         );
-      }
-      public function updateUser($userID){
-         $username = 'batman';
-         $fullName = 'Mr. Wayne';
-         $password = 'I Love Rachel';
-         $this->user_model->updateUser(
-            [
-               'user_username' => $username,
-               'user_full_name' => $fullName,
-               'user_password' => $password,
-            ],$userID,
-         );
-      }
-      public function deleteUser($userID){
-         $this->user_model->deleteUser($userID);
-      }
+      // public function index(){
+      //    $this->load->view('welcome_message');
+      // }
+      // public function show($user){
+      //    $data['result'] = $this->user_model->getUsers($user,1234);
+      //    $this->load->view('user_view',$data);
+      // }
+      // public function addUser(){
+      //    $username = 'spiderman';
+      //    $fullName = 'Peter Parker';
+      //    $password = 'ironman';
+      //    $this->user_model->createUser(
+      //       [
+      //          'user_username'=>$username,
+      //          'user_full_name'=>$fullName,
+      //          'user_password'=>$password,
+      //       ],
+      //    );
+      // }
+      // public function updateUser($userID){
+      //    $username = 'batman';
+      //    $fullName = 'Mr. Wayne';
+      //    $password = 'I Love Rachel';
+      //    $this->user_model->updateUser(
+      //       [
+      //          'user_username' => $username,
+      //          'user_full_name' => $fullName,
+      //          'user_password' => $password,
+      //       ],$userID,
+      //    );
+      // }
+      // public function deleteUser($userID){
+      //    $this->user_model->deleteUser($userID);
+      // }
    }
 ?>
--- application/views/users/login_view.php
+++ application/views/users/login_view.php
@@ -1,10 +1,15 @@
 <h3>User Login</h3>
-<?php $properties = array('id'=>'login_form','class'=>'form_horizontal');?>
-<?php echo form_open("users/login_view",$properties); ?>
+<?php 
+   $properties = array('id'=>'login_form','class'=>'form_horizontal');
+   if($this->session->flashdata('errors')){
+      echo $this->session->flashdata('errors');
+   }
+   echo form_open("users/login",$properties);
+?>
 <div class="form-group mb-3">
    <?php
       $lblAttributes = array('class' => 'sr-only');
-      $usnAttributes = array('class' => 'form-control','placeholder' => 'Enter Your Username', 'type' => 'text', 'name' => 'username', 'id' => 'username');
+      $usnAttributes = array('class' => 'form-control','placeholder' => 'Enter Your Username', 'type' => 'text', 'name' => 'username');
       echo form_label('Username','username',$lblAttributes);
       echo form_input($usnAttributes);
    ?>
@@ -12,7 +17,7 @@
 <div class="form-group mb-3">
    <?php
       $lblAttributes = array('class' => 'sr-only');
-      $pswAttributes = array('class' => 'form-control','placeholder' => 'Enter Your Password', 'type' => 'password', 'name' => 'password', 'id' => 'password');
+      $pswAttributes = array('class' => 'form-control','placeholder' => 'Enter Your Password', 'type' => 'password', 'name' => 'password');
       echo form_label('Password','password',$lblAttributes);
       echo form_input($pswAttributes);
    ?>
